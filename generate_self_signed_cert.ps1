f1cb8dde36dfd0e5eca9f816e6c050c0a1e01ad5
Maciej Jarosiewicz <mjarosie@users.noreply.github.com>
2020-09-24 17:14:14 +0100

    supress the output

--- generate_self_signed_cert.ps1
+++ generate_self_signed_cert.ps1
@@ -43,17 +43,17 @@ $rootCertPathPfx = "certs"
 $identityServerCertPath = "src/IdentityServer/certs"
 $webApiCertPath = "src/Api/certs"
 
-[System.IO.Directory]::CreateDirectory($rootCertPathPfx)
-[System.IO.Directory]::CreateDirectory($identityServerCertPath)
-[System.IO.Directory]::CreateDirectory($webApiCertPath)
+[System.IO.Directory]::CreateDirectory($rootCertPathPfx) | Out-Null
+[System.IO.Directory]::CreateDirectory($identityServerCertPath) | Out-Null
+[System.IO.Directory]::CreateDirectory($webApiCertPath) | Out-Null
 
-Export-PfxCertificate -Cert $testRootCA -FilePath "$rootCertPathPfx/aspnetapp-root-cert.pfx" -Password $password
-Export-PfxCertificate -Cert $identityServerCert -FilePath "$identityServerCertPath/aspnetapp-identity-server.pfx" -Password $password
-Export-PfxCertificate -Cert $webApiCert -FilePath "$webApiCertPath/aspnetapp-web-api.pfx" -Password $password
+Export-PfxCertificate -Cert $testRootCA -FilePath "$rootCertPathPfx/aspnetapp-root-cert.pfx" -Password $password | Out-Null
+Export-PfxCertificate -Cert $identityServerCert -FilePath "$identityServerCertPath/aspnetapp-identity-server.pfx" -Password $password | Out-Null
+Export-PfxCertificate -Cert $webApiCert -FilePath "$webApiCertPath/aspnetapp-web-api.pfx" -Password $password | Out-Null
 
 # Export .cer to be converted to .crt to be trusted within the Docker container.
 $rootCertPathCer = "certs/aspnetapp-root-cert.cer"
-Export-Certificate -Cert $testRootCA -FilePath $rootCertPathCer -Type CERT
+Export-Certificate -Cert $testRootCA -FilePath $rootCertPathCer -Type CERT | Out-Null
 
 # Trust it on your host machine.
 $store = New-Object System.Security.Cryptography.X509Certificates.X509Store "Root","LocalMachine"
